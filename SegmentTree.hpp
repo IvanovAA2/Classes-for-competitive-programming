0eb89b0e56029da65366b7956a1db459f1bd763a
IvanovAA <ivanov_alexandr_05@mail.ru>
2025-09-19 22:54:27 +0300

    Segment Tree rework

--- SegmentTree.hpp
+++ SegmentTree.hpp
@@ -3,108 +3,78 @@ class SegmentTree
 private:
 	struct Node
 	{
-		static constexpr ll sum_ne = 0;		// нейтральный элемент сложения
-		static constexpr ll max_ne = ll(1) << 63; // нейтральный элемент максимума
-		static constexpr ll min_ne = (ll(1) << 63) - 1;	// нейтральный элемент минимума
+		static constexpr ll sum_ne = 0;
+		static constexpr ll max_ne = LLONG_MIN;
+		static constexpr ll min_ne = LLONG_MAX;
+		
+		static constexpr ll cng_ne = 0; // нейтральное значение изменения не отрезке
 
-		static constexpr ll cng_ne = 0; // нейтральный элемент изменения на отрезке
-
-		ll sum;
-		int sz;
-		//ll max;
+		//ll sum;
+		ll max;
 		//ll min;
 
 		ll cng = Node::cng_ne;
 
 		Node() :
-			sum(sum_ne),
-			sz(1)
-			//max(max_ne)
+			//sum(sum_ne)
+			max(max_ne)
 			//min(min_ne)
-		{}
+		{
+		}
 		Node(ll x) :
-			sum(x),
-			sz(1)
-			//max(x)
+			//sum(x)
+			max(x)
 			//min(x)
-		{}
+		{
+		}
 	};
 
-	int p = 1;
+	const int p; 
 	vector<Node> t;
 
 private:
-	inline void update_cng(int x, ll d) // обновление изменения на отрезке
+	void apply(int c, int cl, int cr, ll cng) // приминение изменения на отрезке
 	{
-		t[x].cng += d;
-		//t[x].cng = d; 
-		//t[x].cng = max(t[x].cng, d); 
-		//t[x].cng = min(t[x].cng, d); 
+		//t[c].cng = cng;
+		t[c].cng += cng; 
+		//t[c].cng = max(t[c].cng, cng); 
+		//t[c].cng = min(t[c].cng, cng);
+		
+		t[c].max += cng;
 	}
 
-	inline void propagate(int x)
-	{
-		if (t[x].cng == Node::cng_ne)
-		{
-			return;
-		}
-		// влияние изменения на операицю
-		t[x].sum += t[x].cng * t[x].sz;
-		//t[x].max += t[x].cng;
-		//t[x].min += t[x].cng;
-		//t[x].sum = t[x].cng * t[x].sz;
-
-		if (x < p)
-		{
-			update_cng(2 * x, t[x].cng);
-			update_cng(2 * x + 1, t[x].cng);
-		}
-		t[x].cng = Node::cng_ne;
-	}
-
-	inline Node comb(Node a, Node b) // комбинирование узлов
+	Node comb(Node a, Node b) // комбинирование узлов
 	{
 		Node res;
-		res.sum = a.sum + b.sum;
-		res.sz = a.sz + b.sz;
-		//res.max = max(a.max, b.max);
+
+		//res.sum = a.sum + b.sum;
+		res.max = max(a.max, b.max);
 		//res.min = min(a.min, b.min);
+
 		return res;
 	}
 
-	inline Node get(int x)
+	void propagate(int c, int cl, int cr)
 	{
-		propagate(x);
-		return t[x];
+		if (t[c].cng != Node::cng_ne and cr - cl != 1)
+		{
+			int cm = (cl + cr) / 2;
+			apply(2 * c + 0, cl, cm, t[c].cng);
+			apply(2 * c + 1, cm, cr, t[c].cng);
+
+			t[c].cng = Node::cng_ne;
+		}
 	}
 
-	inline void update(int x)
+	void update(int c)
 	{
-		t[x] = comb(get(x * 2), get(x * 2 + 1));
+		t[c] = comb(t[c * 2 + 0], t[c * 2 + 1]);
 	}
 
 public:
 	SegmentTree(int n, ll x)
+		: p (bit_ceil(size_t(n))), t (2 * p, Node(x))
 	{
-		while (p < n)
-		{
-			p <<= 1;
-		}
-		t.assign(2 * p, Node(x));
-		for (int i = p - 1; i; --i)
-		{
-			update(i);
-		}
-	}
-
-	void read(int n, int from = 0)
-	{
-		for (int i = 0; i < n; ++i)
-		{
-			ll x;
-			cin >> x;
-			t[p + from + i] = Node(x);
-		}
 		for (int i = p - 1; i; --i)
 		{
 			update(i);
@@ -112,20 +82,13 @@ public:
 	}
 
 	template<class S>
-	SegmentTree(const vector<S> &v)
+	SegmentTree(const vector<S>& v)
+		: p (bit_ceil(size(v))), t (2 * p)
 	{
-		int n = size(v);
-		while (p < n)
-		{
-			p <<= 1;
-		}
-		t.assign(2 * p, Node());
-
-		for (int i = 0; i < n; i++)
+		for (int i = 0; i < size(v); i++)
 		{
 			t[p + i] = Node(v[i]);
 		}
-
 		for (int i = p - 1; i; i--)
 		{
 			update(i);
@@ -134,11 +97,12 @@ public:
 
 	Node get(int l, int r, int cl = 0, int cr = -1, int c = 1)
 	{
-		propagate(c);
 		if (cr == -1)
 		{
 			cr = p;
 		}
+		propagate(c, cl, cr);
+
 		if (cl >= r or cr <= l)
 		{
 			return Node();
@@ -149,21 +113,19 @@ public:
 		}
 
 		int cm = (cl + cr) / 2;
-		return comb(get(l, r, cl, cm, c * 2), get(l, r, cm, cr, c * 2 + 1));
+		return comb(
+			get(l, r, cl, cm, c * 2 + 0), 
+			get(l, r, cm, cr, c * 2 + 1)
+		);
 	}
 
-	Node get_node(int i)
+	void change(int l, int r, ll cng, int cl = 0, int cr = -1, int c = 1)
 	{
-		return get(i, i + 1);
-	}
-
-	void change(int l, int r, ll d, int cl = 0, int cr = -1, int c = 1)
-	{
-		propagate(c);
 		if (cr == -1)
 		{
 			cr = p;
 		}
+		propagate(c, cl, cr);
 
 		if (cl >= r or cr <= l)
 		{
@@ -171,13 +133,13 @@ public:
 		}
 		if (cl >= l and cr <= r)
 		{
-			update_cng(c, d);
+			apply(c, cl, cr, cng);
 			return;
 		}
 
 		int cm = (cl + cr) / 2;
-		change(l, r, d, cl, cm, c * 2);
-		change(l, r, d, cm, cr, c * 2 + 1);
+		change(l, r, cng, cl, cm, c * 2 + 0);
+		change(l, r, cng, cm, cr, c * 2 + 1);
 		update(c);
 	}
 };
